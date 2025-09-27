e7e53c7082ecc546f258ce7bf63f02cb1e8a77dd
chandana-1ce23cs030 <chandanavenkatesha@gmail.com>
2025-09-27 16:00:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+   <>
+     <div className=" flex h-screen w-full justify-center items-center bg-lime-600">
+      <div className=" bg-amber-300 p-3 border-2">Hello</div>
+      <div className=" bg-fuchsia-600 p-4 rounded-sm">world</div>
+     </div>
+     </>
+  );
+}
+
+export default App;
---
+++ src/assets/components/Hello.jsx
@@ -0,0 +1,9 @@
+function Hello(params) {
+
+  return (
+  <>
+  <div className="text-2xl text-blue-600">Hello {params.name}</div>;
+  </>
+  );
+}
+export default Hello;
